4c5fc99d4dfa47c5c65948f05cbf3d6bd19cf54b
chawit033 <61050033@kmitl.ac.th>
2021-07-05 15:15:29 +0700

    can pull data

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import bookList from './books.json'
+
+import './main.scss'
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.getElementById('card')
+    const books = bookList.data
+    insertBand(books,cards)
+    console.log(books)
+
+})
+
+function insertBand(books,cards) {
+    books.forEach(book => {
+        let cardDOM = builtCardDOM(book)
+        cards.insertAdjacentHTML('beforeend', cardDOM)
+    })
+
+}
+        
+
+function builtCardDOM(book) {
+    let img = bookList.image_base_url
+    return (
+        `<div class="card">
+            <img src="${img.concat(`${book.ImageURL}`)}" alt="#" />
+            <div class="book-info">   
+                <div class="title">${book.Title}<div>
+                <div class="author">${book.Authors}<div>
+                <div class="format">${book.format}</div>
+            </div>
+        </div>`
+
+    )
+}
