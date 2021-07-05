bc6d1806ac6226132fec84e62eae47ada9f68c6c
chawit033 <61050033@kmitl.ac.th>
2021-07-05 21:50:30 +0700

    remove console

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import bookList from './books.json'
-
 import './main.scss'
 
 document.addEventListener('DOMContentLoaded', function() {
     const cards = document.getElementById('card')
     const books = bookList.data
     insertBook(books,cards)
-    console.log(books)
-
 })
 
 function insertBook(books,cards) {
@@ -15,21 +12,17 @@ function insertBook(books,cards) {
         let cardDOM = builtCardDOM(book)
         cards.insertAdjacentHTML('beforeend', cardDOM)
     })
-
 }
         
-
 function builtCardDOM(book) {
-    let img = bookList.image_base_url
     return (
         `<div class="card">
-            <img src="${img.concat(`${book.ImageURL}`)}" alt="" />
+            <img src="https://images.bwbcovers.com/${book.ImageURL}" alt="#" />
             <div class="book-info">   
                 <div class="title">${book.Title}<div>
                 <div class="author">${book.Authors}<div>
                 <div class="format">${book.Format}</div>
             </div>
         </div>`
-
     )
 }
